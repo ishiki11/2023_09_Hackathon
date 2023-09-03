6c1e01b9fc3d8591f834fa382a22678ac9955b51
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-09-03 12:56:02 +0900

    models中身の修正

--- models/users.py
+++ models/users.py
@@ -2,6 +2,7 @@ from app import db
 
 
 class Users(db.Model):
+  __tablename__ = 'Users'
   id = db.Column(db.Integer, primary_key=True)
   mail = db.Column(db.String(256), unique=True, nullable=False)
   hashed_password = db.Column(db.String(256), nullable=False)
