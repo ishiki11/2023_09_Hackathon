956ffb682e30fcfa2cd7980f120e1dfc652da899
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-09-05 21:54:00 +0900

    python処理作成

---
+++ views/todo_act.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, redirect, render_template
+import DB.todo_act_db as db
+
+# Blueprint登録
+todo_act = Blueprint('todo_act', __name__)
+
+
+# todo実行中処理
+@todo_act.route('/todo/act/<param>', methods=['GET'])
+def act(param):
+  todo = db.get_todo(todo_id=param)
+  if (todo == "error"):
+    return redirect('todo')  # todo一覧にリダイレクトする
+  # テンプレートファイルのtodo_act.html
+  return render_template(
+      'todo_act.html',
+      data=todo
+  )
