1a5131c014f01d6fb323968c906845f252fcb5d3
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-09-07 11:36:52 +0900

    session追加

--- views/todo_act.py
+++ views/todo_act.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template
+from flask import Blueprint, redirect, render_template, session
 import DB.todo_act_db as db
 
 # Blueprint登録
@@ -8,8 +8,13 @@ todo_act = Blueprint('todo_act', __name__)
 # todo実行中処理
 @todo_act.route('/todo_act/<param>', methods=['GET'])
 def act(param):
+  user_id = 1  # loginができたら消す
+  # if (session['id'] != None):
+  #   user_id = session['id']
+  # else:
+  #   return redirect('/login')
 
-  todo = db.get_todo(todo_id=param)
+  todo = db.get_todo(todo_id=param, user_id=user_id)
 
   if (todo == "error"):
     return redirect('todo')  # todo一覧にリダイレクトする
