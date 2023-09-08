77707ceb382f2df922922382022f48ed2665d97e
uemura <m.uemura.sys22@morijyobi.ac.jp>
2023-09-08 15:11:39 +0900

    user_ranking.py修正

--- views/user_ranking.py
+++ views/user_ranking.py
@@ -6,48 +6,56 @@ ranking = Blueprint('ranking', __name__)
 @ranking.route('/user_ranking', methods=['GET'])
 def user_ranking():
   userid = 1 #セッションで持ってくる
+  userrank = user_rank()
   myranking = myrank(userid)
   print(myranking)
-  userrank = user_rank()
+  print(userrank)
   count = 1
   rank_num = 1
   rank = []
-  print(userrank)
   for urank in userrank:
     if count == 1:
       rank.append([rank_num,urank[0],urank[1]])
-    elif userrank[count-2][1] == urank[1]:
+    elif userrank[count-2][1]== urank[1]:
       rank.append([rank_num,urank[0],urank[1]])
     else:
       rank_num = count
       rank.append([rank_num,urank[0],urank[1]])
-  count+=1
+    count+=1
   return render_template('user_ranking.html',rank=rank,myrank=myranking)
 def user_rank():
   sql = "SELECT username, rank_rate FROM Users ORDER BY rank_rate DESC LIMIT 10;"
+  connection = None
+  cursor = None
   try:
     url = os.environ['DATABASE_URL']
     connection = psycopg2.connect(url)
     cursor = connection.cursor()
     cursor.execute(sql,)
     rank = cursor.fetchall()
-  except psycopg2.DatabaseError :
-    rank = "error"
+  except psycopg2.DatabaseError as e:
+    rank = e
   finally :
-    cursor.close()
-    connection.close()
-    return rank
+    if cursor:
+      cursor.close()
+    if connection:
+      connection.close()
+  return rank
 def myrank(userid):
   sql = "SELECT rank_rate FROM Users where id = %s"
+  connection = None
+  cursor = None
   try:
     url = os.environ['DATABASE_URL']
     connection = psycopg2.connect(url)
     cursor = connection.cursor()
-    cursor.execute(sql,(userid,))
-    myrank = cursor.fetchone()
-  except psycopg2.DatabaseError :
-    myrank = "error"
+    cursor.execute(sql, (userid,))
+    myrank = cursor.fetchall()
+  except psycopg2.DatabaseError as e:
+    myrank = e
   finally :
-    cursor.close()
-    connection.close()
-    return myrank
+    if cursor:
+      cursor.close()
+    if connection:
+      connection.close()
+  return myrank
