8307b9e73ca73709397f547a1ade4fa0a9b43fd6
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-09-10 13:27:18 +0900

    updateの場所修正

--- views/password_edit.py
+++ views/password_edit.py
@@ -25,7 +25,6 @@ def pass_edit():
 @password_edit.route('/password_edit', methods=['POST'])
 def pass_edit_exe():
   user_id = session.get('id')
-  error = ""
   user_id = 1
   if user_id is None:
     # ログインへ遷移
@@ -33,6 +32,7 @@ def pass_edit_exe():
   pw = request.form.get('pw', type=str)
   pw2 = request.form.get('pw2', type=str)
 
+  error = ""
   # 入力がされていない項目がある場合
   user = db.id_select_user(user_id)
   if pw == "" or pw2 == "":
@@ -45,20 +45,20 @@ def pass_edit_exe():
   # パスワードチェック
   if pw != pw2:  # pwとpw2が一致しないとき
     error = 'パスワードが一致していません'
-  if len(pw) < 5:  # pw文字数チェック
+  if len(pw) < 4:  # pw文字数チェック
     error = 'パスワートを4文字以上にしてください'
   if not re.search("[a-z]", pw) or not re.search("[A-Z]", pw):
     error = 'パスワードで大文字と小文字を使用してください'
   if not re.match("^[a-zA-Z0-9]+$", pw):
     error = 'パスワードは英数字のみを使用してください'
 
-  flg = db.update_password(user_id, pw)
   old = [pw, pw2]
 
   # エラーがある時
   if error != "":
     return render_template("password_edit.html", error=error, old=old, data=user)
 
+  flg = db.update_password(user_id, pw)
   if flg:
     msg = '変更が完了しました。'
     return render_template('password_edit.html', msg=msg, data=user)
