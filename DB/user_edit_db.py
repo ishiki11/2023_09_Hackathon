633ae46ff6a0858401d7f9af3be2e65ecd90f69f
miyanokengo <113155752+miyanokengo@users.noreply.github.com>
2023-09-11 10:26:38 +0900

    edit,registerの作成

--- DB/user_edit_db.py
+++ DB/user_edit_db.py
@@ -8,6 +8,24 @@ def get_connection():
     return connection
 
 
+# ランダムなソルトを生成
+def get_salt():
+
+# 文字列の候補(英大小文字+ 数字)
+    charset = string.ascii_letters + string.digits
+
+# charset からランダムに30文字取り出して結合
+    salt = ''.join(random.choices(charset, k=30))
+    return salt
+
+# ソルトとPWからハッシュ値を生成
+def get_hash(pw, salt):
+    b_pw = bytes(pw, "utf-8")
+    b_salt = bytes(salt, "utf-8")
+    hashed_password = hashlib.pbkdf2_hmac("sha256", b_pw, b_salt, 1000).hex()
+    return hashed_password
+
+
 def id_select_user(id):
     row=None
     sql = 'SELECT mail,username FROM Users where id = %s'
@@ -31,12 +49,17 @@ def update_user(id, mail, pw, username):
     count=-1
     sql = 'UPDATE Users SET mail = %s, hashed_password = %s, username = %s WHERE id = %s'
 
+# ハッシュ化
+    salt = get_salt() # ソルトの生成
+    hashed_password = get_hash(pw, salt) # 生成したソルトでハッシュ
+    
+    
     try:
         connection = get_connection()
 
         cursor = connection.cursor()
 
-        cursor.execute(sql, (mail, pw, username, id))
+        cursor.execute(sql, (mail,hashed_password, username, id))
 
         count = cursor.rowcount
 
--- views/user_edit.py
+++ views/user_edit.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, Blueprint,request, redirect, url_for, session
-import string, random
+import string, random, re
 import DB.user_edit_db as db
 user_edit = Blueprint('user_edit', __name__, '/user_edit')
 
@@ -7,21 +7,12 @@ user_edit = Blueprint('user_edit', __name__, '/user_edit')
 # ログイン処理
 @user_edit.route('/user_edit')
 def edit():
+  user_id = session.get('id')
+  if user_id is None:
+    # ログインへ遷移
+    return redirect('/')
 
-  # print("A")
-
-  # if 'id' in session:
-  #       return render_template('edit.html')
-  # else :
-  #       return redirect(url_for('login.index'))
-
-
-
-  row = db.id_select_user(session['id'])
-
-
-
-  #sessionの処理したい
+  row = db.id_select_user(user_id)
 
   print(row)
 
@@ -30,20 +21,54 @@ def edit():
 
 @user_edit.route('/edit_exe', methods=['POST'])
 def edit_exe():
-    user_id = session['id']
-    mail = request.form.get('mail')
-    pw = request.form.get('pw')
-    username = request.form.get('username')
-    print(user_id,pw,mail,username)
+  user_id = session.get('id')
+  if user_id is None:
+    # ログインへ遷移
+    return redirect('/')
+  mail = request.form.get('mail')
+  pw = request.form.get('pw')
+  pw2 = request.form.get('pw2')
+  username = request.form.get('username')
+  print(user_id,pw,mail,username)
+  
+  row = (mail, username)
 
-    count = db.update_user(user_id, mail, pw, username)
 
-    row = db.id_select_user(id)
+  if username == '' or pw == '' or mail == '' or pw2 == '':
+      a = '入力されていない項目があります'
+      return render_template('user_edit.html',a=a,row=row)
 
-    print(count)
-    if count == 1:
-        msg = '変更が完了しました。'
-        return render_template('user_edit.html',msg=msg,row=row)
-    else:
-        error = '変更出来ませんでした'
-        return render_template('user_edit.html', error=error,row=row)
+
+  if pw != pw2:
+      error = 'パスワードが一致していません'
+      return render_template('user_edit.html', error=error,row=row)
+
+  if len(pw) < 4:
+    error = 'パスワートを4文字以上にしてください'
+    return render_template('user_edit.html',error=error,row=row)
+
+  if not re.search("[a-z]", pw) or not re.search("[A-Z]", pw):
+    error = 'パスワードは大文字、小文字の英数字のみを使用してください'
+    return render_template('user_edit.html',error=error,row=row)
+
+  if not re.match("^[a-zA-Z0-9]+$", pw):
+    error = 'パスワードは英数字のみを使用してください'
+    return render_template('user_edit.html',error=error,row=row)
+
+  if len(mail) > 255 or len(username) > 255 or len(pw) > 255:
+  # 文字数
+    error = "入力文字数が多すぎます"
+    return render_template('user_register.html',error=error,row=row)
+
+
+  count = db.update_user(user_id, mail, pw, username)
+
+  row = db.id_select_user(user_id)
+
+  print(count)
+  if count == 1:
+      msg = '変更が完了しました。'
+      return render_template('user_edit.html',msg=msg,row=row)
+  else:
+      error = '変更出来ませんでした'
+      return render_template('user_edit.html', error=error,row=row)
