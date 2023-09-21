ae012a63467aa369ab03194c8493999e980946fa
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2023-09-21 14:53:01 +0900

    画面表示の作成

--- views/admin_top.py
+++ views/admin_top.py
@@ -4,3 +4,24 @@ import DB.admin_top_db as db
 admin_top = Blueprint('admin_top', __name__)
 
 
+# トップ画面表示
+@admin_top.route('/admin/top', methods=['GET'])
+def user_list():
+  try:
+    users = db.get_users()
+  except Exception as e:
+    print("Exception", e)
+  return render_template('admin_top.html', users=users)
+
+
+# ユーザ削除用
+@admin_top.route('/admin/delete/<param>', methods=['GET'])
+def user_delete(param):
+  if param is None:
+    return redirect('/admin/top')
+  user_id = param
+  try:
+    db.delete_user(user_id)
+  except Exception as e:
+    print("Exception", e)
+  return redirect('/admin/top')
